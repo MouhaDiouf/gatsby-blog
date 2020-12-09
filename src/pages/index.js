e87e631ba159f00e0280d6beccd639e7e5e03812
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-09 16:01:26 +0000

    Create the blog page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
-export default function Home() {
-  return <div>The Great Gatsby Bootcamp</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  )
 }
+
+export default IndexPage
