822752e35731166eeaca2f3f07c60f6ad13917e2
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-10 13:11:43 +0000

    Show the blog content on the page

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,26 +1,30 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+    contentfulGastbyApp(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        raw
       }
-      html
     }
   }
 `
 function BlogPost(props) {
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div
-        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-      ></div>
+      <h1>{props.data.contentfulGastbyApp.title}</h1>
+      <p>{props.data.contentfulGastbyApp.publishedDate}</p>
+      <p>
+        {" "}
+        {documentToReactComponents(
+          JSON.parse(props.data.contentfulGastbyApp.body.raw)
+        )}
+      </p>
     </Layout>
   )
 }
